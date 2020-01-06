9fb452703f642d72db2d6b1ec885c36a34e1861a
Dmitry <total@techbuben.ru>
2020-01-06 13:50:14 +0300

    init

---
+++ config/deploy.rb
@@ -0,0 +1,63 @@
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rbenv'
+require 'mina/nginx'
+require 'mina/puma'
+# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
+# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
+
+set :application_name, 'Cheshir_pro'
+set :domain, '127.0.0.1'
+set :deploy_to, '/home/depus/app_deploy'
+set :repository, 'git@github.com:Cheshir74/MCS.git'
+set :branch, 'master'
+set :port, '2222'
+set :user, 'depus'
+set :shared_dirs,  fetch(:shared_dirs, []).push('tmp', 'log', 'public/uploads', 'public/system')
+set :shared_files, fetch(:shared_files, []).push('config/puma.rb', 'config/database.yml', 'config/secrets.yml')
+
+#set :rails_env, 'production'
+
+task :remote_environment do
+  invoke :'rbenv:load'
+end
+
+task :setup do
+  command %{mkdir -p "#{fetch(:shared_path)}/log"}
+  command %{chmod g+rx,u+rwx "#{fetch(:shared_path)}/log"}
+
+  command %{mkdir -p "#{fetch(:shared_path)}/config"}
+  command %{chmod g+rx,u+rwx "#{fetch(:shared_path)}/config"}
+
+  command %{touch "#{fetch(:shared_path)}/config/puma.rb"}
+  command %{touch "#{fetch(:shared_path)}/config/database.yml"}
+  command %{touch "#{fetch(:shared_path)}/config/secrets.yml"}
+
+  command %{mkdir -p "#{fetch(:shared_path)}/tmp/sockets"}
+  command %{chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/sockets"}
+  command %{mkdir -p "#{fetch(:shared_path)}/tmp/pids"}
+  command %{chmod g+rx,u+rwx "#{fetch(:shared_path)}/tmp/pids"}
+end
+
+task :deploy do
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+    invoke :'deploy:cleanup'
+
+
+    on :launch do
+      invoke :'puma:restart'
+    end
+  end
+end
