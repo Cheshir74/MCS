a91a8b1bc539a1f522c20a3ce298571d2f644558
Dmitry <total@techbuben.ru>
2020-06-13 05:13:18 +0500

    active storage

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,17 @@
 class Gallery < ApplicationRecord
-  has_many :images, dependent: :destroy
+  #has_many :images, dependent: :destroy
+  has_many_attached :images
+
+  def self.sizes
+    {
+        thumbnail: { resize: "100x100" },
+        hero1:     { resize: "1000x500" }
+    }
+  end
+
+  def sized(size)
+    self.images.variant(Image.sizes[size]).processed
+  end
+
+
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,15 +1,9 @@
 class Image < ApplicationRecord
   belongs_to :gallery
 
-  mount_uploader :image, ImageUploader
+  #mount_uploader :image, ImageUploader
+  has_one_attached :image
 
-  before_save :update_image_attributes
 
-  private
-  def update_image_attributes
-    if image.present? && image_changed?
-      self.image_content_type = image.file.content_type
-      self.image_file_size = image.file.size
-    end
-  end
+
 end
