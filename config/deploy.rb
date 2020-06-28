af699330beec761b5c25b177068f415548722c60
Dmitry <total@techbuben.ru>
2020-06-29 01:23:09 +0500

    review views; fix deploy.rb;

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :branch, 'master'
 set :port, '22'
 #set :port, '9022'
 set :user, 'depus'
-set :shared_dirs,  fetch(:shared_dirs, []).push('tmp', 'log', 'public/uploads', 'public/system')
+set :shared_dirs,  fetch(:shared_dirs, []).push('tmp', 'log', 'public/uploads', 'public/system', 'storage')
 set :shared_files, fetch(:shared_files, []).push('config/puma.rb', 'config/database.yml', 'config/master.key', 'config/secrets.yml')
 set :bundle_options, -> { '' }
 
--- db/migrate/20200621085511_remove_column_to_homes.rb
+++
@@ -1,7 +0,0 @@
-class RemoveColumnToHomes < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :homes, :image
-    remove_column :homes, :image_file_size
-    remove_column :homes, :image_content_type
-  end
-end
