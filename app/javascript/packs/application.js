61d18cdd36ceaa3186e94fb06a2b81304e875b9f
Dmitry <total@techbuben.ru>
2020-07-05 16:25:27 +0500

    remove unused js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,22 +4,20 @@
 // that code so it'll be compiled.
 import "bootstrap"
 import "../stylesheets/application"
-
 import './src/application.scss'
 import toastr from 'toastr'
 window.toastr = toastr
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+
 require("jquery")
 
-
+require("channels")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'css/site.scss'
