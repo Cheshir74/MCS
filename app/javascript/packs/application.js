5c4b1ca8438023d1ec46c4460b826e3bc7ebbb4d
Cheshir <total@techbuben.ru>
2020-07-25 01:46:06 +0300

    relocation files css for webpack true

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "bootstrap"
-import "../stylesheets/application"
-import './src/application.scss'
+
 import toastr from 'toastr'
 window.toastr = toastr
 
@@ -12,6 +10,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,3 +18,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap";
+import "./src/application.scss";
