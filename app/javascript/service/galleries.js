25d1a0e4d3d44bd336d5e74c6add3d6c74cf070a
Cheshir <total@techbuben.ru>
2020-07-29 19:53:28 +0300

    migrate lightgallery to yarn node_modules

--- app/javascript/service/galleries.js
+++ app/javascript/service/galleries.js
@@ -1,5 +1,10 @@
 $(document).on('turbolinks:load', function () {
     $('#lightgallery').lightGallery({
-        selector: '.item'
+        selector: '.item',
+        escKey: true,
+        hideControlOnEnd: true,
+        preload: 2,
+        download: false,
+        pager: true
     });
 });
