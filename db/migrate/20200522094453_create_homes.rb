f394a916fbbb657ca24d4bcea929154145701a70
Cheshir <total@techbuben.ru>
2020-08-08 12:46:52 +0300

    fix migration

--- db/migrate/20200522094453_create_homes.rb
+++ db/migrate/20200522094453_create_homes.rb
@@ -1,7 +1,8 @@
 class CreateHomes < ActiveRecord::Migration[6.0]
   def change
     create_table :homes do |t|
-
+      add_column :homes, :title, :string
+      add_column :homes, :body, :text
       t.timestamps
     end
   end
