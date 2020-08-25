fb046ab31cf07b56ad76408a442c153daf4d9b42
Cheshir <total@techbuben.ru>
2020-08-25 23:05:48 +0300

    add fix function for admin_panel with webpack

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -11,14 +11,19 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-
+require('lightgallery/src/js/lightgallery')
+require('lg-fullscreen/src/lg-fullscreen')
+require('lg-pager/src/lg-pager')
+require('lg-hash/src/lg-hash')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "lightgallery/dist/css/lightgallery.css";
 import "bootstrap";
 import 'stylesheets/admin'
 
+import 'service/galleries';
 
