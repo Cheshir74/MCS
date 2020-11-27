b1edb9275b7a0d5ea5953a2a61012a15243258a8
Cheshir74 <total@techbuben.ru>
2020-11-27 19:13:49 +0300

    add dropzone

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -12,6 +12,9 @@ require('lg-fullscreen/src/lg-fullscreen')
 require('lg-pager/src/lg-pager')
 require('lg-hash/src/lg-hash')
 
+import "dropzone/dist/min/dropzone.min.css";
+import "dropzone/dist/min/basic.min.css";
+import "controllers"
 import "lightgallery/dist/css/lightgallery.css";
 import "bootstrap";
 import 'stylesheets/admin'
