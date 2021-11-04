fdba40d32ac70761d6c5dd91fa34ad82750095fc
Cheshir74 <total@techbuben.ru>
2021-11-05 00:01:51 +0300

    react gallery

--- app/javascript/packs/gallery_react.jsx
+++ app/javascript/packs/gallery_react.jsx
@@ -1,24 +1,60 @@
-import React from "react";
+import React,{useState,useEffect, useCallback } from "react";
 import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 import photos from "./images.json"
+import Carousel, { Modal, ModalGateway } from "react-images";
 import axios from "axios";
 
-class TodoApp extends React.Component {
+function App() {
+    const [isLoading, setLoading] = useState(true);
+    const [images,setImages]=useState();
+    const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    getTodoItems() {
-        axios
-        .get("/api/v1/todo_items")
-        .then(response => {
-            const todoItems = response.data;
-            this.setState({ todoItems });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
     
-}
+    useEffect(()=>{
+        axios.get("2.json").then(response => {
+            setImages(response.data)
+            setLoading(false);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <div className="App"> Loading... </div>;
+    }
+
+    return (
+        <div>
+            
+            <Gallery photos={images} onClick={openLightbox} direction={"column"} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                    <Carousel
+                    currentIndex={currentImage}
+                    views={photos.map(x => ({
+                        ...x,
+                        srcset: x.srcSet,
+                        caption: x.title
+                    }))}
+                    />
+                </Modal>
+                ) : null}
+            </ModalGateway>
+            </div>
+        );
+    
+  }
+
+
 /* popout the browser and maximize to see more columns! -> */
-const App = () => <Gallery photos={photos} direction={"column"} />;
 render(<App />, document.getElementById("app"));
