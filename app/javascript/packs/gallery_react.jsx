f86c4398506c8781da638fbb08b5bd24db1b5c2d
Cheshir74 <total@techbuben.ru>
2021-11-10 16:26:23 +0300

    fix render react

--- app/javascript/packs/gallery_react.jsx
+++ app/javascript/packs/gallery_react.jsx
@@ -2,62 +2,74 @@ import React,{useState,useEffect, useCallback } from "react";
 import { render } from "react-dom";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
-import axios from "axios";
 
-function App(node) {
-    const [isLoading, setLoading] = useState(true);
-    const [photos,setImages]=useState();
-    const url = node.link;
-    const [currentImage, setCurrentImage] = useState(0);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+function App() {
+    const node = document.getElementById('app')
+    const url = node.getAttribute('data-url-path')+'.json';
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [photos, setItems] = useState([]);
+		const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-  const openLightbox = useCallback((event, { photo, index }) => {
-    setCurrentImage(index);
-    setViewerIsOpen(true);
-  }, []);
-
-  const closeLightbox = () => {
-    setCurrentImage(0);
-    setViewerIsOpen(false);
-  };
-    
-    useEffect((id)=>{
-        axios.get(url).then(response => {
-            setImages(response.data)
-            setLoading(false);
-        });
+    const openLightbox = useCallback((event, { photo, index }) => {
+      setCurrentImage(index);
+      setViewerIsOpen(true);
     }, []);
+    const closeLightbox = () => {
+      setCurrentImage(0);
+      setViewerIsOpen(false);
+    };
 
-    if (isLoading) {
-        return <div className="App"> Loading... </div>;
-    }
-    console.log(photos);
-    return (
+    useEffect(() => {
+			const controller = new AbortController();
+			const signal = controller.signal;
+			fetch(url, {
+				signal: signal
+			})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+			return () => {
+				controller.abort();
+			};
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
         <div>
-            
-            <Gallery photos={photos} onClick={openLightbox} direction={"column"} />
+          <Gallery photos={photos} onClick={openLightbox} direction={"column"} />
             <ModalGateway>
-                {viewerIsOpen ? (
+              {viewerIsOpen ? (
                 <Modal onClose={closeLightbox}>
-                    <Carousel
+                  <Carousel
                     currentIndex={currentImage}
                     views={photos.map(x => ({
-                        ...x,
-                        srcset: x.srcSet,
-                        caption: x.title
+                      ...x,
+                      srcset: x.srcSet,
+                      caption: x.title
                     }))}
-                    />
+                  />
                 </Modal>
-                ) : null}
+              ) : null}
             </ModalGateway>
-            </div>
-        );
-    
+        </div>
+      );
+    }
   }
 
-$(document).on('turbolinks:load', function () {
-    const node = document.getElementById('app')
-    const link = node.getAttribute('data-url-path')
-    const url = link+'.json';
-    render(<App link={url} /> , document.getElementById("app"))
-  })
+
+render(<App /> , document.getElementById("app"))
+
