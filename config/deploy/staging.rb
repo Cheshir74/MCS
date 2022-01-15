410c60e14e83855951b4bec4f9c02059f6378f6a
Cheshir74 <total@techbuben.ru>
2022-01-16 01:00:15 +0300

    change deploy file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,6 +4,7 @@
 # You can define all roles on a single server, or split them:
 server "192.168.11.248", user: "depus", port: "2222", roles: %w{app db web}
 set :stage, :production
+set :branch, 'new_version_design'
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
