8ef137d2e21ce3112e3b1ce70ee7d5e204f9db75
Cheshir74 <total@techbuben.ru>
2024-01-02 02:28:12 +0500

    fix changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "MCS_production"
-  config.action_mailer.default_url_options = { host: Rails.application.secrets.mail_domain }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.mailer.mail_domain }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
