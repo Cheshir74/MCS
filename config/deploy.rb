84d698bdff76e5cfc746f60e1ef7face4cba195a
Cheshir74 <total@techbuben.ru>
2024-01-02 22:28:05 +0500

    err?

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,4 +5,16 @@ set :application, 'TolstosheevPhoto'
 set :deploy_to, '/home/depus/app_deploy'
 set :repo_url, 'git@github.com:Cheshir74/MCS.git'
 append :linked_files, "config/database.yml", "config/master.key", "config/credentials.yml.enc"
-append :linked_dirs, "log", "tmp", "public/system", "storage"
+append :linked_dirs, "log", "tmp", "public/system", "storage", "tmp/pids", "tmp/cache", "tmp/sockets", "public/packs", ".bundle", "node_modules"
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
