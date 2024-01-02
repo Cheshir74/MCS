e4f40eb58d8f730185a3eb63b22dc34e267691ba
Cheshir74 <total@techbuben.ru>
2024-01-02 22:34:23 +0500

    fff

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'TolstosheevPhoto'
 set :deploy_to, '/home/depus/app_deploy'
 set :repo_url, 'git@github.com:Cheshir74/MCS.git'
 append :linked_files, "config/database.yml", "config/master.key", "config/credentials.yml.enc"
-append :linked_dirs, "log", "tmp", "public/system", "storage", "tmp/pids", "tmp/cache", "tmp/sockets", "public/packs", ".bundle", "node_modules"
+append :linked_dirs, "log", "tmp", "public/system", "storage"
 
 before "deploy:assets:precompile", "deploy:yarn_install"
 namespace :deploy do
