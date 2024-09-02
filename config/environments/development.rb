804c6f9506f9c2fe0b83ee73b60e4ec226530c1f
Cheshir74 <total@techbuben.ru>
2024-09-02 21:56:48 +0300

    update front

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,22 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "test.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "0.0.0.0:3000" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      ssl:  Rails.application.credentials.mailer.mail_ssl,
+      tls:  Rails.application.credentials.mailer.mail_tls,
+      user_name:      Rails.application.credentials.mailer.mail_username,
+      password:       Rails.application.credentials.mailer.mail_password,
+      address:       Rails.application.credentials.mailer.mail_address,
+      port:          Rails.application.credentials.mailer.mail_port,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,20 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "mcs_production"
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.mailer.mail_domain }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      ssl:  Rails.application.credentials.mailer.mail_ssl,
+      tls:  Rails.application.credentials.mailer.mail_tls,
+      user_name:      Rails.application.credentials.mailer.mail_username,
+      password:       Rails.application.credentials.mailer.mail_password,
+      address:       Rails.application.credentials.mailer.mail_address,
+      port:          Rails.application.credentials.mailer.mail_port,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
