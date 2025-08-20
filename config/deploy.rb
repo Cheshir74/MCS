34960ef5f9a46159d56121fd880aa7e7ea89bec1
cheshir <total@techbuben.ru>
2025-08-20 21:45:34 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,67 @@
-# config valid for current version and patch releases of Capistrano
+# config valid for current version of Capistrano
 lock "~> 3.17.3"
 
 set :application, 'Photohub'
-set :deploy_to, '/home/depus/app_deploy'
 set :repo_url, 'git@github.com:Cheshir74/MCS.git'
-append :linked_files, "config/database.yml", "config/master.key", "config/secrets.yml", "config/credentials.yml.enc"
-append :linked_dirs, "log", "tmp", "public/system", "storage"
-namespace :deploy do
-    desc 'Run migrations'
-    task :migrate do
-      on roles(:app) do
-        within release_path do
-          execute :rake, 'db:migrate'
-        end
-      end
-    end
-  
-    desc 'Run jsbundling-rails build'
-    task :build_js_assets do
-      on roles(:web) do
-        within release_path do
-          execute :yarn, 'install --immutable'
-          execute :yarn, 'run build'
-        end
-      end
-    end
+set :deploy_to, '/home/depus/app_deploy'
 
-    after 'deploy:updated', 'deploy:build_js_assets'
+# Linked files and dirs
+append :linked_files, "config/database.yml", "config/master.key", "config/credentials/production.key"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
+
+# Ruby environment
+set :rbenv_type, :user
+set :rbenv_ruby, '3.3.0'
+
+# Default environment variables
+set :default_env, {
+  'RAILS_MASTER_KEY' => File.read("#{shared_path}/config/master.key").strip
+}
+
+# Puma settings
+set :puma_threads, [4, 16]
+set :puma_workers, 2
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{shared_path}/log/puma.access.log"
+set :puma_error_log, "#{shared_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
+namespace :deploy do
+  desc 'Run DB migrations'
+  task :migrate do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, 'exec rake db:migrate'
+        end
+      end
+    end
   end
-  
-  # Правильная последовательность выполнения задач
-  before 'deploy:assets:precompile', 'deploy:migrate'
+
+  desc 'Build JS assets'
+  task :build_js_assets do
+    on roles(:web) do
+      within release_path do
+        execute :yarn, 'install --immutable'
+        execute :yarn, 'run build'
+      end
+    end
+  end
+
+  desc 'Restart Puma'
+  task :restart do
+    on roles(:app) do
+      invoke 'puma:restart'
+    end
+  end
+end
+
+# Hooks sequence
+after 'deploy:updated', 'deploy:migrate'
+after 'deploy:updated', 'deploy:build_js_assets'
+after 'deploy:migrate', 'deploy:assets:precompile'
+after 'deploy:publishing', 'deploy:restart'
