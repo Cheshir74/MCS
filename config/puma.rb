65d7b4c3336ca69b5260dc68ab6f32d3d0d80448
cheshir <total@techbuben.ru>
2025-08-24 22:14:49 +0300

    fix puma log

--- config/puma.rb
+++ config/puma.rb
@@ -3,12 +3,12 @@ shared_dir = "/home/depus/app_deploy/shared"
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
+
 environment ENV.fetch("RAILS_ENV") { "production" }
 
 pidfile "#{shared_dir}/tmp/pids/puma.pid"
 state_path "#{shared_dir}/tmp/pids/puma.state"
-stdout_redirect "#{shared_dir}/tmp/log/puma.stdout.log", "#{shared_dir}/tmp/log/puma.stderr.log", true
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
