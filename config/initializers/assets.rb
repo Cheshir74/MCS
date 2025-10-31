16b8b514969dd7bec830fd3240df7ef4bc2d5711
cheshir <total@techbuben.ru>
2025-10-31 23:12:04 +0300

    fix fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,9 @@ Rails.application.config.assets.version = "2.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font","node_modules/@fortawesome/fontawesome-free/webfonts", 'app', 'assets', 'builds')
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "bootstrap-icons", "font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "@fortawesome", "fontawesome-free", "webfonts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "builds")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
