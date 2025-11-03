a6b17a99aa8dac236b2152d18060a8a2deea70eb
cheshir <total@techbuben.ru>
2025-11-03 14:55:11 +0300

    fix deploy

--- config/puma.rb
+++ config/puma.rb
@@ -1,20 +1,23 @@
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "/home/depus/app_deploy/shared"
+app_dir = "/home/depus/app_deploy"
+shared_dir = "#{app_dir}/shared"
+current_dir = "#{app_dir}/current"
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-pidfile "#{shared_dir}/tmp/pids/puma.pid" if ENV["RAILS_ENV"] == "production"
-state_path "#{shared_dir}/tmp/pids/puma.state" if ENV["RAILS_ENV"] == "production"
-
 if ENV["RAILS_ENV"] == "production"
+  directory current_dir
+  rackup "#{current_dir}/config.ru"
+
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
+
   stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
   bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }.to_i
+  preload_app!
 end
-
-workers ENV.fetch("WEB_CONCURRENCY") { 2 } if ENV["RAILS_ENV"] == "production"
-preload_app! if ENV["RAILS_ENV"] == "production"
 
 plugin :tmp_restart
