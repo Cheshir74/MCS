b323b29532cd12e3f7e5e581f1c2c4e6707d5e82
cheshir <total@techbuben.ru>
2026-08-27 22:59:55 +0300

    Fix Kamal production boot

--- config/puma.rb
+++ config/puma.rb
@@ -2,6 +2,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
 environment ENV.fetch("RAILS_ENV") { "development" }
+port ENV.fetch("PORT") { 3000 }
 
 if ENV["RAILS_ENV"] == "production"
   pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
